0b8dab1dc670e6b4b5113b0b5d81a03cf3af3c8e
Jeffrey198501 <43401505+Jeffrey198501@users.noreply.github.com>
2019-04-07 11:01:27 +0800

    Update Test01.java

--- src/main/java/zzz/Test01.java
+++ src/main/java/zzz/Test01.java
@@ -5,6 +5,7 @@ public class JAVA01 {
 	public static void main(String args[]) {
 		// TODO Auto-generated method stub
 		 int just01 = 1;
+                 int just02 = 2;
 		 char thing = 'T';
 		 String your = "Mine";
 		 if (just01 == 1){
